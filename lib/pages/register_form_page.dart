a8ef76749066fd2405aa38c8e1034df593e4cd12
Glebovfl <GlebovFlutter@yandex.ru>
2022-01-09 18:34:55 +0300

    start validating

--- lib/pages/register_form_page.dart
+++ lib/pages/register_form_page.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'dart:io';
+
 // void main() {
 //   runApp(MyApp());
 // }
 
 class RegisterFormPage extends StatefulWidget {
-
   @override
   _RegisterFormPageState createState() => _RegisterFormPageState();
 }
@@ -14,6 +15,8 @@ class RegisterFormPage extends StatefulWidget {
 class _RegisterFormPageState extends State<RegisterFormPage> {
   bool _hidePass = true;
   bool _hidePassConf = true;
+
+  final _formKey = GlobalKey<FormState>();
 
   final _nameController = TextEditingController();
   final _phoneController = TextEditingController();
@@ -41,18 +44,19 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
         centerTitle: true,
       ),
       body: Form(
+        key: _formKey,
         child: ListView(
           padding: EdgeInsets.all(16.0),
           children: [
-            TextField(
+            TextFormField(
               controller: _nameController,
               decoration: InputDecoration(
-                labelText: 'Full Name *',
-                hintText: 'Школьная кличка',
-                prefixIcon: Icon(Icons.person),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
+                  labelText: 'Full Name *',
+                  hintText: 'Школьная кличка',
+                  prefixIcon: Icon(Icons.person),
+                  suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -61,20 +65,22 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  )
-              ),
+                  )),
+              validator: _validateName,
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             TextFormField(
               controller: _phoneController,
               decoration: InputDecoration(
-                labelText: 'Phone Number *',
-                hintText: 'Есть позвонить?',
-                helperText: 'Format: (XXX)XXX-XXXX',
-                prefixIcon: Icon(Icons.call),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
+                  labelText: 'Phone Number *',
+                  hintText: 'Есть позвонить?',
+                  helperText: 'Format: (XXX)XXX-XXXX',
+                  prefixIcon: Icon(Icons.call),
+                  suffixIcon: Icon(
+                    Icons.delete_outline,
+                    color: Colors.red,
                   ),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
@@ -83,14 +89,17 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(20.0)),
                     borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                  )
-              ),
+                  )),
               keyboardType: TextInputType.phone,
               inputFormatters: [
                 FilteringTextInputFormatter.digitsOnly,
               ],
+              validator: (val) => val!.isEmpty ? 'Name is required' : null,
+
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             TextFormField(
               controller: _emailController,
               decoration: InputDecoration(
@@ -100,17 +109,25 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
               ),
               keyboardType: TextInputType.emailAddress,
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             TextFormField(
               controller: _storyController,
               decoration: InputDecoration(
-                labelText: 'True Story *', alignLabelWithHint: true,
+                labelText: 'True Story *',
+                alignLabelWithHint: true,
                 hintText: 'Выкладывай...',
                 border: OutlineInputBorder(),
               ),
               maxLines: 3,
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(100),
+              ],
             ),
-            SizedBox(height: 10.0,),
+            SizedBox(
+              height: 10.0,
+            ),
             TextFormField(
               controller: _passController,
               obscureText: _hidePass,
@@ -119,57 +136,73 @@ class _RegisterFormPageState extends State<RegisterFormPage> {
                 labelText: 'Password *',
                 hintText: 'Пароль введи',
                 suffixIcon: IconButton(
-                  icon: Icon(_hidePass ? Icons.visibility : Icons.visibility_off),
+                  icon:
+                      Icon(_hidePass ? Icons.visibility : Icons.visibility_off),
                   onPressed: () {
                     setState(() {
                       _hidePass = !_hidePass;
                     });
                   },
-                  ),
+                ),
                 icon: Icon(Icons.security),
               ),
             ),
-            SizedBox(height: 10.0,),
-           TextFormField(
-             controller: _confirmPassController,
+            SizedBox(
+              height: 10.0,
+            ),
+            TextFormField(
+              controller: _confirmPassController,
               obscureText: _hidePassConf,
               maxLength: 8,
               decoration: InputDecoration(
                 labelText: 'Confirm Password *',
                 hintText: 'Пароль повтори',
                 suffixIcon: IconButton(
-                  icon: Icon(_hidePassConf ? Icons.visibility : Icons.visibility_off),
+                  icon: Icon(
+                      _hidePassConf ? Icons.visibility : Icons.visibility_off),
                   onPressed: () {
                     setState(() {
                       _hidePassConf = !_hidePassConf;
                     });
                   },
-                  ),
+                ),
                 icon: Icon(Icons.security),
               ),
             ),
-            SizedBox(height: 20.0,),
+            SizedBox(
+              height: 20.0,
+            ),
             RaisedButton(
               onPressed: _submitForm,
               color: Colors.green,
-              child: Text(
-                'Зарегистрироваться',
-                style: TextStyle(color: Colors.white)
-              ),
+              child: Text('Зарегистрироваться',
+                  style: TextStyle(color: Colors.white)),
             )
           ],
         ),
-        ),
+      ),
     );
   }
 
   void _submitForm() {
+    if(_formKey.currentState!.validate()){
+    print('Form is valid');
+
     print('name user is ${_nameController.text}');
     print('phone user is ${_phoneController.text}');
     print('email user is ${_emailController.text}');
     print('story user is ${_storyController.text}');
-    print('pass user is ${_passController.text}');
-    print('confirmpass user is ${_confirmPassController.text}');
+    }
+  }
 
+  String? _validateName(String value) {
+    final _nameExp = RegExp(r'^[A-Za-z ]+$');
+    if(value.isEmpty){
+      return 'Name is reqired.';
+    } else if(!_nameExp.hasMatch(value)){
+      return 'Please enter alphabetical characters.';
+    } else {
+      return null;
+    }
   }
 }
